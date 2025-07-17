98ff14327d0100f0f2e42f6c3f5d3fc2fcf9a527
s-moshe <kassasalim4@gmail.com>
2025-07-17 02:53:45 +0000

    Added the repo and photos for dawgdealz

---
+++ script.js
@@ -0,0 +1,12 @@
+// scripts.js
+function openLightbox(img) {
+  const lightbox = document.getElementById('lightbox');
+  const lightboxImg = document.getElementById('lightbox-img');
+  lightbox.style.display = 'flex';
+  lightboxImg.src = img.src;
+  lightboxImg.alt = img.alt;
+}
+
+function closeLightbox() {
+  document.getElementById('lightbox').style.display = 'none';
+}
